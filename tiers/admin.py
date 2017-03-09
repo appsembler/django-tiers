6220618ac3a8d1f77c47ef24b8f8bbc5e69213a4
Deni Bertovic <deni@denibertovic.com>
2017-03-09 19:54:54 +0100

    Adds better admin list

--- tiers/admin.py
+++ tiers/admin.py
@@ -4,5 +4,18 @@ from django.contrib import admin
 from .models import Tier
 
 
-admin.site.register(Tier)
+def make_exempt(modeladmin, request, queryset):
+    queryset.update(tier_enforcement_exempt=True)
+make_exempt.short_description = "Exempt from tier enforcement"
+
+
+class TierAdmin(admin.ModelAdmin):
+    list_display = ('organization', 'name', 'tier_expires_at',
+            'tier_enforcement_grace_period', 'tier_enforcement_exempt',)
+    search_fields = ['organization__name', 'name']
+    list_filter = ['name', 'tier_expires_at']
+    actions = [make_exempt]
+
+
+admin.site.register(Tier, TierAdmin)
 
