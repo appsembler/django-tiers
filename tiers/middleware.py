bff55535bb06335f2dc8eedd005512aeade8ca0e
Anders Pearson <anders@thraxil.org>
2020-06-17 16:25:20 +0100

    add basic honeycomb instrumentation
    with dummied out functions so it will still work if `beeline` isn't
    installed.

--- tiers/middleware.py
+++ tiers/middleware.py
@@ -7,6 +7,10 @@ from django.utils.deprecation import MiddlewareMixin
 from .models import Tier
 from .app_settings import EXPIRED_REDIRECT_URL, ORGANIZATION_TIER_GETTER_NAME
 
+try:
+    import beeline
+except ImportError:
+    import tiers.instrumentation as beeline
 
 log = logging.getLogger(__name__)
 
@@ -15,6 +19,7 @@ class TierMiddleware(MiddlewareMixin):
     """
     Django Tiers middleware
     """
+    @beeline.traced(name="TiersMiddleware.process_request")
     def process_request(self, request):
         """
         Fetch organization from session and deny access to the system if the tier
@@ -22,6 +27,7 @@ class TierMiddleware(MiddlewareMixin):
         """
         # If we're aleady on the url where we have to be, do nothing
         if (EXPIRED_REDIRECT_URL is not None) and (request.path.rstrip('/') in EXPIRED_REDIRECT_URL.rstrip('/')):
+            beeline.add_context_field("tiers.no_action_required", True)
             return
 
         # try/catch needed because the URLs don't necessarily exist both in AMC and edX
@@ -43,9 +49,11 @@ class TierMiddleware(MiddlewareMixin):
         # If there is no organization in the sesssion fail silenty.
         # This should not happen.
         if not request.session.get('organization'):
+            beeline.add_context_field("tiers.no_organization", True)
             return
 
         org = request.session['organization']
+        beeline.add_context_field("tiers.organization", "{}".format(org))
         try:
             if ORGANIZATION_TIER_GETTER_NAME:
                 tier = org.__getattribute__(ORGANIZATION_TIER_GETTER_NAME)()
@@ -55,6 +63,7 @@ class TierMiddleware(MiddlewareMixin):
             # If the organization for some reason does not have a tier assigned
             # fail silently. This should not happen. We should always automatically create
             # a tier for each organization.
+            beeline.add_context_field("tiers.organization_without_tier", True)
             log.warning("Organization wihout Tier: {0}".format(org))
             return
 
@@ -62,10 +71,13 @@ class TierMiddleware(MiddlewareMixin):
         request.session['DISPLAY_EXPIRATION_WARNING'] = ((tier.name == Tier.TIERS.TRIAL) and
                                                          (not tier.tier_enforcement_exempt))
         request.session['TIER_EXPIRES_IN'] = tier.time_til_tier_expires()
+        beeline.add_context_field("tiers.tier_expires_in", request.session['TIER_EXPIRES_IN'])
         # TODO: I'm not sure if we have to refresh the session info at this point somehow.
         request.session['TIER_EXPIRED'] = tier.has_tier_expired()
+        beeline.add_context_field("tiers.tier_expired", request.session['TIER_EXPIRED'])
         # TODO: We should use request.TIER_NAME instead of meddling the session, but being consistent for now
         request.session['TIER_NAME'] = tier.name
+        beeline.add_context_field("tiers.tier_name", tier.name)
 
         # TODO: I'm not sure if we have to refresh the session info at this point somehow.
         if tier.has_tier_expired():
